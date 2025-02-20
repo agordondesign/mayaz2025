a175f98d336ba6f2e2f495ad96bf30960252718a
Yahya Gordon <andre@agordondesign.com>
2025-02-20 18:30:42 -0500

    showroom-ref/page.tsx
    disable ShowroomFeature component

--- app/(pages)/showroom-ref/page.tsx
+++ app/(pages)/showroom-ref/page.tsx
@@ -1,10 +1,10 @@
-import ShowroomFeature from '@/components/layout/ShowroomFeature';
+//import ShowroomFeature from '@/components/layout/ShowroomFeature';
 import React from 'react';
 
 export default async function page() {
 	return (
 		<div className="flex flex-col gap-16 w-full max-w-[95rem] px-2 mx-auto">
-			<ShowroomFeature />
+			{/*<ShowroomFeature />*/}
 			<header>
 				<h1 className="text-center text-2xl lg:text-2xl uppercase tracking-[0.75em]">
 					<span className="block text-base lg:text-xl tracking-[0.5em] pb-3 text-mayaz font-light">
