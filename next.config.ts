9116b42e529269365988747407a1cf1addcd4f63
Yahya Gordon <andre@agordondesign.com>
2025-02-24 06:27:54 -0500

    VCF redirect
    redirect /Maÿaz_Collection.vcf to ‘/‘

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+	async redirects() {
+		return [
+			{
+				source: '/Maÿaz_Collection.vcf',
+				destination: '/',
+				permanent: true,
+			},
+		];
+	},
 	images: {
 		remotePatterns: [
 			{
